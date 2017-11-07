570858fce6ac177e52bad134a3c98e743cbbe0be
Son Nguyen <nvs2394@gmail.com>
2017-11-07 21:37:45 +0700

    Fix env

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export IMAGE_NAME="${REPO_NAME:-hapi-starter-kit}:${REPO_TAG:-latest}"
+export IMAGE_NAME="${REPO_NAME:-api-v1}:${REPO_TAG:-latest}"
 docker-compose -f 'docker-compose.test.yml' -p ci up --build --abort-on-container-exit
 exit $(docker wait ci_hapi-starter-kit_1)
 
